7adb6ff064c7502ebe19f4ccc584b0eac5f0e724
hachi399 <yonghachi399@gmail.com>
2025-06-29 16:21:34 +0900

    Update server.js

--- server.js
+++ server.js
@@ -6,14 +6,16 @@ const wss = new WebSocket.Server({ port: PORT }, () => {
 });
 
 wss.on('connection', (ws) => {
-  ws.on('message', (message) => {
-    // 全員に送信（ブロードキャスト）
-    wss.clients.forEach(client => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
+  console.log('クライアントが接続しました')
+  ws.on('message', message => {
+  const text = message.toString(); // Buffer → string に変換
+  server.clients.forEach(client => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(text); // 明示的に文字列を送信
+    }
   });
+});
+
 
   ws.send('接続成功！チャットを始めましょう。');
 });
